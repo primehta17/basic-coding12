5881062365db3ed080a7b5f6c06fc6eb09f8d672
Priyanka Mehta <primehta17@gmail.com>
2025-04-06 00:51:48 +0530

    empty the array

--- five.js
+++ five.js
@@ -2,12 +2,12 @@
 
 let arr =[1,2,3];
 
-arr.length=0;
+arr.length=0;//Empty Array by Setting Length 0
 
 console.log(arr);
 
 let arr2 =[4,5,6];
 
-arr2=[];
+arr2=[];//Empty Array by Substituting New Array
 
 console.log(arr2);
